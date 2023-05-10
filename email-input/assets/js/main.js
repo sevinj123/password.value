378e86715c2c2384b2f2169bbc4b87a65165768d
sevinj123 <127213193+sevinj123@users.noreply.github.com>
2023-05-10 17:27:55 +0400

    Add files via upload

---
+++ email-input/assets/js/main.js
@@ -0,0 +1,49 @@
+// //inputdan kod ve emailin geliisini qebul etsin
+// const email=document.querySelector(".email");
+// const password=document.querySelector(".password");
+// function clickme(){
+//     console.log(toUpperCase(password.value),password.value);
+//    if(!(email.value.includes("@"))){
+//     email.style.borderColor="red"
+//    }
+
+//    if(!(password.value.length >= 6 && toUpperCase(password.value) && toLoverCase(password.value) && checkNum(password.value))){
+//     password.style.borderColor="red"
+//    }
+   
+// }   
+
+
+// function toUpperCase(str){
+//     for(let i=0; i< str.length; i++){
+//         if (checkNum(str[i])) {
+//             continue;            
+//         }
+//         if(str[i] === str[i].toUpperCase()){
+//             return  true;
+//         }
+//     }
+//     return false;
+// }
+
+// function toLoverCase(str){
+//     for(let i=0; i<str.length; i++){
+//         if (checkNum(str[i])) {
+//             continue;
+//         }
+//         if( str[i] === str[i].toLowerCase()){
+//             return  true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// function checkNum(str){
+//     for(let i=0; i<str.length; i++){
+//         if(!isNaN(str[i])){
+//             return  true;
+//         }
+//     }
+//     return false;
+// }
